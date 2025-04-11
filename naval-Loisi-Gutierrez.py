f513e160188711d1700bd499544ff7249230a895
millieloisi <167239633+millieloisi@users.noreply.github.com>
2025-04-11 12:07:49 -0300

    tablero y barcos

--- naval-Loisi-Gutierrez.py
+++ naval-Loisi-Gutierrez.py
@@ -1,22 +1,26 @@
-#tablero
+#tablero y esconder barcos
 
-print("  0 1 2 3 4 5 6 7 8 9")
+def show_board(Barco, Nobarco, completo):
+    print("  0 1 2 3 4 5 6 7 8 9")
 
-hit = [21, 22]
-miss = [20, 24, 12, 13]
-comp = [23]
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = "  "
+            if place in Nobarco:
+                ch = " x"
+            elif place in Barco:
+                ch = " o"
+            elif place in completo:
+                ch = " 0"
+            row += ch
+            place += 1
+        print(x, " ", row)
 
-place = 0
-for x in range(10):
-    row = ""
-    for y in range(10):
-        ch = "  "
-        if place in miss:
-            ch = " x"
-        elif place in hit:
-            ch = " o"
-        elif place in comp:
-            ch = " 0"
-        row = row + ch
-        place = place + 1
-    print(x, " ", row)
+
+Barco = [21, 22]
+Nobarco = [20, 24, 12, 13]
+completo = [23]
+
+show_board(Barco, Nobarco, completo)
