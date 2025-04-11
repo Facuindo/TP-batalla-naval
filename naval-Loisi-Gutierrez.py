408f76d6abc25d3b478fdda8653d8915b9392b0f
millieloisi <167239633+millieloisi@users.noreply.github.com>
2025-04-11 10:29:49 -0300

    Tablaero casi terminado

--- naval-Loisi-Gutierrez.py
+++ naval-Loisi-Gutierrez.py
@@ -1,8 +1,22 @@
 #tablero
 
-print(" 1 2 3 4 5 6 7 8 9 10 ")
+print("  0 1 2 3 4 5 6 7 8 9")
 
-for x in range (10):
+hit = [21, 22]
+miss = [20, 24, 12, 13]
+comp = [23]
+
+place = 0
+for x in range(10):
     row = ""
-    for y in range (10):
-print(x, " ", " _ ")
+    for y in range(10):
+        ch = "  "
+        if place in miss:
+            ch = " x"
+        elif place in hit:
+            ch = " o"
+        elif place in comp:
+            ch = " 0"
+        row = row + ch
+        place = place + 1
+    print(x, " ", row)
